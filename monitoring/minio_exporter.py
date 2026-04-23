10eb42dd09e2ada222cbec9f154c3dd925585747
Hardik Amarwani <82609732+Hard-Hustler@users.noreply.github.com>
2026-04-22 20:08:24 -0400

    Remove default MINIO_ENDPOINT value
    Updated MINIO_ENDPOINT to use environment variable without default.

--- monitoring/minio_exporter.py
+++ monitoring/minio_exporter.py
@@ -19,7 +19,7 @@ SCAN_DURATION       = Histogram('data_scan_duration_seconds', 'Time to scan a fi
 LAST_SCAN_TIMESTAMP = Gauge('data_last_scan_timestamp', 'Unix timestamp of last scan', ['bucket'])
 
 BUCKET = os.getenv('MINIO_BUCKET', 'zulip-rewriter')
-MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT', '129.114.27.192.nip.io') 
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT') 
 ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY')
 SECRET_KEY = os.getenv('MINIO_SECRET_KEY')
 
